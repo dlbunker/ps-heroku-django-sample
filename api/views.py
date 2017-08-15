7bca69c6f91f0438bba55b57aab156fe3b7824e5
Dan Bunker <dan.bunker@stgconsulting.com>
2017-08-15 00:00:40 -0600

    returning response as son

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ import coreapi
 from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework.views import APIView
 
 class RiverViewSet(APIView):
@@ -12,4 +13,4 @@ class RiverViewSet(APIView):
     renderer_classes = (JSONRenderer, )
 
     def get(self, request, format=None):
-        return Response(coreapi.Client().get('http://riverbrain.com/api/v1/rivers'))
+        return JsonResponse(coreapi.Client().get('http://riverbrain.com/api/v1/rivers'), safe=False)
